4ede5ef68f0fe326877ec8f7dd92e087c16c1e11
Dmitry Sitnikov <dim4s14@gmail.com>
2019-11-26 22:19:43 +0300

    Added HM

---
+++ lesson2/merge_sort.py
@@ -0,0 +1,43 @@
+from random import randint
+
+
+def mergeSort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+
+
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+
+mergeSort(a)
+print(a)
